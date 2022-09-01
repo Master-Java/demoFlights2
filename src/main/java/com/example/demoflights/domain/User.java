c526705b9a230df2896c0ba929c7e15769685684
Igor Alekseev <i.alekseev@iac.spb.ru>
2022-09-01 13:20:40 +0300

    Update

--- src/main/java/com/example/demoflights/domain/User.java
+++ src/main/java/com/example/demoflights/domain/User.java
@@ -1,10 +1,14 @@
 package com.example.demoflights.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String phone;
     private String surname;
